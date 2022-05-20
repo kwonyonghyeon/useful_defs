35ba67d38c8c4d400bb6beb8b89d74d41a4a2eb7
Ropher Yonghyeon Kwon <87412818+kwonyonghyeon@users.noreply.github.com>
2022-05-20 17:55:09 +0900

    Update gcd_EuclideanAlgorithm().py

--- Python/gcd_EuclideanAlgorithm().py
+++ Python/gcd_EuclideanAlgorithm().py
@@ -1,6 +1,6 @@
 #gcd_EuclideanAlgorithm() 함수는 유클리드 호제법을 이용하여 최대공약수(Greatest Common Divisor, GCD)를 구하는 함수입니다.
 
-def gcd(a, b):
+def gcd_EuclideanAlgorithm(a, b):
     while b > 0:
         a, b = min(a, b), max(a, b)
         a, b = a, int(b%a)
